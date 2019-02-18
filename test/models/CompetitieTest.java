f14c29ab1166523b34fede7d6cd7d385367ef513
R.N.A. Vlaar <ronald.vlaar@hva.nl>
2019-02-18 21:24:57 +0100

    Rectificatie van de Asserts bij CompetitieTest + duiding van het testplan

--- test/models/CompetitieTest.java
+++ test/models/CompetitieTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Roan Ettema
+ * @author Roan Ettema & Ronald Vlaar
  */
 
 public class CompetitieTest {
@@ -29,19 +29,34 @@ public class CompetitieTest {
 
     @Before
     public void setUp() throws Exception {
+        //Aanmaken competitie
         instanceCompetitie = new Competitie("Schaaktoernooi 1", LocalDate.of(2019, 2, 10),
                 LocalDate.of(2019, 2, 15));
+
+        //Aanmaken leden die in deze test zullen figureren als ingeschreven leden van de hierboven aangemaakte competitie
         instanceLID1 = new Lid("Roan Ettema", LocalDate.of(1998, 4, 22), 2450);
         instanceLID2 = new Lid("Ronald Vlaar", LocalDate.of(2000,  12, 22), 2450);
+
+        //Aanmaken van het lid dat niet ingeschreven en dus niet toegevoegd wordt aan de instanceCompetitie
         instanceGeenLid = new Lid("Levin Ettema", LocalDate.of(2000, 10, 21), 0);
+
         instanceRonde1 = new Ronde(LocalDate.of(2018, 2, 22));
         instanceRonde2 = new Ronde(LocalDate.of(2018, 2, 23));
+
+        //Partij met uitsluitend leden die ook ingeschreven staan bij de competitie
         instancePartijMetLid = new Partij(instanceLID1, instanceLID2);
         instancePartijGeenLid = new Partij(instanceLID1, instanceGeenLid);
 
+        //Het toevoegen van deze leden gebeurt hieronder
         instanceCompetitie.voegLidToe(instanceLID1);
         instanceCompetitie.voegLidToe(instanceLID2);
+
+        //Ronde1 is in dit testplan de ronde die alleen partijen bevat waarvan alle spelers een ingschrijving hebben
+        //bij de competitie. De ronde zal dus succesvol toegevoegd moeten kunnen worden
         instanceRonde1.voegPartijToe(instancePartijMetLid);
+
+        //Ronde2 bevat een partij met een speler zonder inschrijving en moet dus niet toegevoegd kunnen worden aan
+        //de competitie
         instanceRonde2.voegPartijToe(instancePartijGeenLid);
 
     }
@@ -61,30 +76,35 @@ public class CompetitieTest {
 
     @Test
     public void isNogGeenLid() {
-        System.out.println("Test is wel Lid");
-        boolean expResult = true;
+        System.out.println("\nTest methode isNogGeenLid():");
+        System.out.println("Verwachtingswaarde false, want het lid is ook lid van de competitie");
         boolean result =  instanceCompetitie.isNogGeenLid(instanceLID1);
-        assertEquals(expResult, result);
+        assertFalse(result);
 
         result =  instanceCompetitie.isNogGeenLid(instanceLID2);
-        assertEquals(expResult, result);
-
-        System.out.println("Test is geen Lid");
-        result = instanceCompetitie.isNogGeenLid(instanceGeenLid);
         assertFalse(result);
+
+        System.out.println("Verwachtingswaarde true, immmers is het lid geen lid van de competitie en " +
+                "zou dus moeten gelden dat methode isNogGeenLid == true");
+        result = instanceCompetitie.isNogGeenLid(instanceGeenLid);
+        assertTrue(result);
 
     }
 
     @Test
     public void isGeenRondeMetSpelersZonderInschrijving() {
-        System.out.println("Test partij met leden");
-        boolean expResult = true;
-        boolean result = instanceCompetitie.isGeenRondeMetSpelersZonderInschrijving(instanceRonde1);
-        assertEquals(expResult, result);
 
-        System.out.println("Test partij geen lid");
+        System.out.println("Test methode isGeenRondeMetSpelersZonderInschrijving():");
+        System.out.println("Test met een ronde waarbij de spelers die voorkomen in de partijen lid zijn van de competitie");
+        boolean expResultTrue = true;
+        boolean result = instanceCompetitie.isGeenRondeMetSpelersZonderInschrijving(instanceRonde1);
+        assertEquals(expResultTrue, result);
+
+        System.out.println("Test met een ronde waarbij niet alle spelers die voorkomen in de partijen, ook lid zijn van " +
+                "de competitie");
+        boolean expResultFalse = false;
         result = instanceCompetitie.isGeenRondeMetSpelersZonderInschrijving(instanceRonde2);
-        assertEquals(expResult, result);
+        assertEquals(expResultFalse, result);
 
 
 
