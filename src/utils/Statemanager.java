155ce2ed90884c7ad926ab467ec7ed9dad650e60
Roan <roan.ettema@hva.nl>
2019-03-08 10:43:13 +0100

    opzet statem

---
+++ src/utils/Statemanager.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Statemanager {
+
+    public static  final  int NO_VIEW = 0;
+    public static  final  int Lid_VIEW = 1;
+    public static  final  int Partij_VIEW = 2;
+    public static  final  int Comepetitie_VIEW = 3;
+
+}
